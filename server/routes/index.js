63d42483752af517cf1164344fa57096f140ca09
iykeevans <elochi238@gmail.com>
2018-12-02 14:12:31 +0100

    (chore) mocha: set up mocha [finishes #162366503]

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ import incidents from './incident';
 
 const router = express.Router();
 
-router.get('/api/v1', (req, res) => res.json({ message: 'welocome to me API' }));
+router.get('/api/v1', (req, res) => res.json({ message: 'welcome to me API' }));
 
 router.use('/api/v1', incidents);
 
